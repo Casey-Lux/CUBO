72b6ced164c4713b35f79fd03099a5ef86bef88e
03BrayanM <bhmeneses@unicauca.edu.co>
2024-09-26 18:27:43 -0500

    function insert customer almost finish

--- canchacubo/crearpromocion.Designer.cs
+++ canchacubo/crearpromocion.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(234, 49);
+            this.label1.Location = new System.Drawing.Point(324, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(184, 25);
             this.label1.TabIndex = 1;
